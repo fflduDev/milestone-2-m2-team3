c8cf9abfe9e54ce4801d461aaea8ed36e82e0488
jansalafia <144460301+jansalafia@users.noreply.github.com>
2025-02-26 13:41:10 -0500

    initial commit

--- Contact.java
+++ Contact.java
@@ -1,5 +1,10 @@
 package template;
 
+import java.util.List;
+import java.util.ArrayList;
+
+
+
 /**
  * model a Contact 
  * Contact has a name & list of phonebook entries
@@ -11,6 +16,20 @@ package template;
  */
 
 class Contact {
-	 
+
+    private String name;
+    private List<PhonebookEntry> phonebookEntries = new ArrayList<PhonebookEntry>();
+
+    public Contact(String name) {
+        this.name = name;
+    }
+
+    public List<PhonebookEntry> getPhonebookEntries() {
+        return phonebookEntries;
+    }
+
+    public void addPhonebookEntry(String phoneNumber, String type) {
+        phonebookEntries.add(new PhonebookEntry(phoneNumber, type));
+    }
 
 }
--- PhonebookEntry.java
+++ PhonebookEntry.java
@@ -1,4 +1,3 @@
-package template;
 
 /**
  *   model a PhonebookEntry 
@@ -11,6 +10,29 @@ package template;
 
 class PhonebookEntry{
 
+    private String phoneNumber;
+    private String type;
  
+
+    public PhonebookEntry(String phoneNumber, String type) {
+        this.phoneNumber = phoneNumber;
+        this.type = type;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String setPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+    
+
+
+
 }
 	
